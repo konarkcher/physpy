1f4ffff36da22648f6a5569a615dd227189f62bb
Konarkcher <konarkcher@yandex.ru>
2017-09-29 12:46:55 +0300

    implemented ExperimentValue, added to ols

--- physpy/experimentvalue.py
+++ physpy/experimentvalue.py
@@ -2,43 +2,40 @@ import math
 
 
 class ExperimentValue:
-    def __init__(self, value, absolute_error=0.0, relative_error=0.0):
-        self.value = value
+    def __init__(self, value, sigma=0.0, eps=0.0):
+        self.value = value  # TODO: add physical rounding
 
-        if absolute_error and relative_error:
+        if sigma and eps:
             raise ValueError()
 
-        self.absolute_error = absolute_error
-        self.relative_error = relative_error
-
-        if absolute_error:
-            self.relative_error = absolute_error / value
-
-        if relative_error:
-            self.absolute_error = value * relative_error
+        self.sigma = value * eps if eps else sigma
+        self.eps = sigma / value if sigma else eps
 
     def __add__(self, other):
-        sigma = math.sqrt(self.absolute_error**2 + other.absolute_error**2)
+        sigma = math.sqrt(self.sigma**2 + other.sigma**2)
         return ExperimentValue(self.value + other.value, sigma)
 
     def __sub__(self, other):
-        sigma = math.sqrt(self.absolute_error**2 + other.absolute_error**2)
+        sigma = math.sqrt(self.sigma**2 + other.sigma**2)
         return ExperimentValue(self.value - other.value, sigma)
 
     def __mul__(self, other):
-        pass
+        eps = math.sqrt(self.eps**2 + other.eps**2)
+        return ExperimentValue(self.value * other.value, eps=eps)
 
     def __truediv__(self, other):
-        pass
+        eps = math.sqrt(self.eps**2 + other.eps**2)
+        return ExperimentValue(self.value / other.value, eps=eps)
 
     def __pow__(self, power, modulo=None):
-        pass
+        if modulo is not None:
+            raise ValueError
+        return ExperimentValue(self.value**power, eps=self.eps * power)
 
     def __str__(self):
-        pattern = '{}, sigma = {}, eps = {:.2f}%'
-        return pattern.format(self.value, self.absolute_error,
-                              self.relative_error * 100)
+        pattern = '{:.3f}, sigma = {:.3f}, eps = {:.2f}%'
+        return pattern.format(self.value, self.sigma, self.eps * 100)
 
 
 def sqrt(ev):
-    pass
+    return ExperimentValue(math.sqrt(ev.value), eps=0.5 * ev.eps)
--- physpy/ols.py
+++ physpy/ols.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+from physpy.experimentvalue import ExperimentValue
+
 
 def through_zero(x, y):
     k = np.average(x * y) / np.average(x**2)
@@ -8,18 +10,17 @@ def through_zero(x, y):
     under_sqrt = np.average(y**2) / np.average(x**2) - k**2
     sigma = np.sqrt(np.abs(under_sqrt) / len(x))
 
-    return k, sigma
+    return ExperimentValue(k, sigma)
 
 
 def linear(x, y):
-    k = (np.average(x * y) - np.average(x) * np.average(y)) / (
-        np.average(x**2) - np.average(x)**2)
+    k = ((np.average(x * y) - np.average(x) * np.average(y)) /
+         (np.average(x**2) - np.average(x)**2))
     b = np.average(y) - k * np.average(x)
 
-    under_sqrt = (np.average(y**2) - np.average(y)**2) / (
-        np.average(x**2) - np.average(x)**2) - k**2
+    under_sqrt = ((np.average(y**2) - np.average(y)**2) /
+                  (np.average(x**2) - np.average(x)**2)) - k**2
     sigma_k = np.sqrt(np.abs(under_sqrt) / len(x))
-
     sigma_b = sigma_k * np.sqrt(np.abs(np.average(x**2) - np.average(x)**2))
 
-    return k, b, sigma_k, sigma_b
+    return ExperimentValue(k, sigma_k), ExperimentValue(b, sigma_b)
