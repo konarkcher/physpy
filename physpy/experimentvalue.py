ff9420e8c809803ce6cea8ab2f09057eb6c0e0e8
Konarkcher <konarkcher@yandex.ru>
2017-09-29 15:02:06 +0300

    changed eps to epsilon

--- physpy/experimentvalue.py
+++ physpy/experimentvalue.py
@@ -3,14 +3,14 @@ import copy
 
 
 class ExperimentValue:
-    def __init__(self, value, sigma=0.0, eps=0.0):
+    def __init__(self, value, sigma=0.0, epsilon=0.0):
         self._value = value  # TODO: add physical rounding
 
-        if sigma and eps:
+        if sigma and epsilon:
             raise ValueError()
 
-        self._sigma = abs(value * eps) if eps else abs(sigma)
-        self._eps = abs(sigma / value) if sigma else abs(eps)
+        self._sigma = abs(value * epsilon) if epsilon else abs(sigma)
+        self._epsilon = abs(sigma / value) if sigma else abs(epsilon)
 
     @property
     def value(self):
@@ -21,8 +21,8 @@ class ExperimentValue:
         return self._sigma
 
     @property
-    def eps(self):
-        return self._eps
+    def epsilon(self):
+        return self._epsilon
 
     def __add__(self, other):
         sigma = math.sqrt(self.sigma**2 + other.sigma**2)
@@ -33,17 +33,17 @@ class ExperimentValue:
         return ExperimentValue(self.value - other.value, sigma)
 
     def __mul__(self, other):
-        eps = math.sqrt(self.eps**2 + other.eps**2)
-        return ExperimentValue(self.value * other.value, eps=eps)
+        eps = math.sqrt(self.epsilon**2 + other.epsilon**2)
+        return ExperimentValue(self.value * other.value, epsilon=eps)
 
     def __truediv__(self, other):
-        eps = math.sqrt(self.eps**2 + other.eps**2)
-        return ExperimentValue(self.value / other.value, eps=eps)
+        eps = math.sqrt(self.epsilon**2 + other.epsilon**2)
+        return ExperimentValue(self.value / other.value, epsilon=eps)
 
     def __pow__(self, power, modulo=None):
         if modulo is not None:
             raise ValueError()
-        return ExperimentValue(self.value**power, eps=self.eps * power)
+        return ExperimentValue(self.value**power, epsilon=self.epsilon * power)
 
     def __neg__(self):
         return ExperimentValue(-self.value, self.sigma)
@@ -55,9 +55,9 @@ class ExperimentValue:
         return ExperimentValue(abs(self.value), self.sigma)
 
     def __str__(self):
-        pattern = '{:.3f}, sigma = {:.3f}, eps = {:.2f}%'
-        return pattern.format(self.value, self.sigma, self.eps * 100)
+        pattern = '{:.3f}, sigma = {:.3f}, epsilon = {:.2f}%'
+        return pattern.format(self.value, self.sigma, self.epsilon * 100)
 
 
 def sqrt(ev):
-    return ExperimentValue(math.sqrt(ev.value), eps=0.5 * ev.eps)
+    return ExperimentValue(math.sqrt(ev.value), epsilon=0.5 * ev.epsilon)
