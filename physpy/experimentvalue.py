d6800edbca8cac5d415edc520d4016c222195ade
Konarkcher <konarkcher@yandex.ru>
2017-09-29 14:31:18 +0300

    changed to properties

--- physpy/experimentvalue.py
+++ physpy/experimentvalue.py
@@ -12,51 +12,52 @@ class ExperimentValue:
         self._sigma = abs(value * eps) if eps else abs(sigma)
         self._eps = abs(sigma / value) if sigma else abs(eps)
 
-    def get_value(self):
+    @property
+    def value(self):
         return self._value
 
-    def get_sigma(self):
+    @property
+    def sigma(self):
         return self._sigma
 
-    def get_eps(self):
+    @property
+    def eps(self):
         return self._eps
 
     def __add__(self, other):
-        sigma = math.sqrt(self.get_sigma()**2 + other.get_sigma()**2)
-        return ExperimentValue(self.get_value() + other.get_value(), sigma)
+        sigma = math.sqrt(self.sigma**2 + other.sigma**2)
+        return ExperimentValue(self.value + other.value, sigma)
 
     def __sub__(self, other):
-        sigma = math.sqrt(self.get_sigma()**2 + other.get_sigma()**2)
-        return ExperimentValue(self.get_value() - other.get_value(), sigma)
+        sigma = math.sqrt(self.sigma**2 + other.sigma**2)
+        return ExperimentValue(self.value - other.value, sigma)
 
     def __mul__(self, other):
-        eps = math.sqrt(self.get_eps()**2 + other.get_eps()**2)
-        return ExperimentValue(self.get_value() * other.get_value(), eps=eps)
+        eps = math.sqrt(self.eps**2 + other.eps**2)
+        return ExperimentValue(self.value * other.value, eps=eps)
 
     def __truediv__(self, other):
-        eps = math.sqrt(self.get_eps()**2 + other.get_eps()**2)
-        return ExperimentValue(self.get_value() / other.get_value(), eps=eps)
+        eps = math.sqrt(self.eps**2 + other.eps**2)
+        return ExperimentValue(self.value / other.value, eps=eps)
 
     def __pow__(self, power, modulo=None):
         if modulo is not None:
-            raise NotImplementedError()
-        return ExperimentValue(self.get_value()**power,
-                               eps=self.get_eps() * power)
+            raise ValueError
+        return ExperimentValue(self.value**power, eps=self.eps * power)
 
     def __neg__(self):
-        return ExperimentValue(-self.get_value(), self.get_sigma())
+        return ExperimentValue(-self.value, self.sigma)
 
     def __pos__(self):
         return copy.copy(self)
 
     def __abs__(self):
-        return ExperimentValue(abs(self.get_value()), self.get_sigma())
+        return ExperimentValue(abs(self.value), self.sigma)
 
     def __str__(self):
         pattern = '{:.3f}, sigma = {:.3f}, eps = {:.2f}%'
-        return pattern.format(self.get_value(), self.get_sigma(),
-                              self.get_eps() * 100)
+        return pattern.format(self.value, self.sigma, self.eps * 100)
 
 
 def sqrt(ev):
-    return ExperimentValue(math.sqrt(ev.get_value()), eps=0.5 * ev.get_eps())
+    return ExperimentValue(math.sqrt(ev.value), eps=0.5 * ev.eps)
