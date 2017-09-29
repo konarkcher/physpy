47c3ce6b749c667beba47e4f435829f6baf52e3d
Konarkcher <konarkcher@yandex.ru>
2017-09-29 16:25:07 +0300

    updated plot()

--- physpy/pyplot.py
+++ physpy/pyplot.py
@@ -1,8 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.ticker import MultipleLocator
 
 
-def plot(x, y):
+def plot(x, y, title='', xlabel='x', ylabel='y', save=False):
+    fig = plt.figure(figsize=(10, 6))
+    axes = plt.axes(title=title, xlabel=xlabel, ylabel=ylabel)
+
+    axes.grid(which='major', visible=True, linewidth=1.0)
+
     plt.plot(x, y)
     plt.show()
+
+    if save:
+        if not title:
+            title = 'temp'
+        fig.savefig('{}.png'.format(title), dpi=300)
