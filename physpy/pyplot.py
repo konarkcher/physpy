0295c831466e07b5c244905c9ca1509ae706ed55
Konarkcher <konarkcher@yandex.ru>
2017-11-14 11:37:52 +0300

    added minor ticks to draw_grid()

--- physpy/pyplot.py
+++ physpy/pyplot.py
@@ -1,13 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from matplotlib.ticker import MultipleLocator
+from matplotlib.ticker import AutoMinorLocator
 
 
 def draw_grid(title='', xlabel='x', ylabel='y'):
     fig = plt.figure(figsize=(10, 6))
     axes = plt.axes(title=title, xlabel=xlabel, ylabel=ylabel)
 
+    axes.xaxis.set_minor_locator(AutoMinorLocator(5))
+    axes.yaxis.set_minor_locator(AutoMinorLocator(5))
+
+    axes.grid(which='minor', visible=True, linewidth=0.5)
     axes.grid(which='major', visible=True, linewidth=1.0)
 
     return fig, axes
