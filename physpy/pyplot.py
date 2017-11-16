7cfe7112e3100f9686d3b64f0aa0d474381fbc62
Konarkcher <konarkcher@yandex.ru>
2017-11-16 10:33:46 +0300

    added utils.linear_plot() and pyplot.linear()

--- physpy/pyplot.py
+++ physpy/pyplot.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 from matplotlib.ticker import AutoMinorLocator
 
+import physpy as pp
+
 
 def create_fig(title='', xlabel='x', ylabel='y'):
     fig = plt.figure(figsize=(10, 6))
@@ -13,3 +15,14 @@ def create_fig(title='', xlabel='x', ylabel='y'):
     axes.grid(which='major', visible=True, linewidth=1.0)
 
     return fig, axes
+
+
+def linear(x, y, through_zero=True, points=True, yerr=None, xerr=None):
+    if points:
+        if yerr is None or xerr is None:
+            plt.plot(x, y, 'bo')
+        else:
+            plt.errorbar(x, y, fmt='b,', yerr=yerr, xerr=xerr)
+
+    plt.plot(*pp.utils.linear_plot(x, y, through_zero=through_zero), 'b-')
+    return pp.utils.ols(x, y, through_zero=through_zero)
--- physpy/utils.py
+++ physpy/utils.py
@@ -3,14 +3,14 @@ import numpy as np
 import physpy as pp
 
 
-def linear(x, y, through_zero=True):
+def ols(x, y, through_zero=True):
     if through_zero:
         k = np.average(x * y) / np.average(x**2)
 
         under_sqrt = np.average(y**2) / np.average(x**2) - k**2
         sigma = np.sqrt(np.abs(under_sqrt) / len(x))
 
-        return pp.ExperimentValue(k, sigma)
+        return pp.ExperimentValue(k, sigma), pp.ExperimentValue(0)
 
     k = ((np.average(x * y) - np.average(x) * np.average(y)) /
          (np.average(x**2) - np.average(x)**2))
@@ -22,6 +22,14 @@ def linear(x, y, through_zero=True):
     sigma_b = sigma_k * np.sqrt(np.abs(np.average(x**2) - np.average(x)**2))
 
     return pp.ExperimentValue(k, sigma_k), pp.ExperimentValue(b, sigma_b)
+
+
+def linear_plot(x, y, through_zero=True):
+    k, b = ols(x, y, through_zero=through_zero)
+
+    delta = (x.max() - x.min()) / 50
+    lin_x = np.array([x.min() - delta, x.max() + delta])
+    return lin_x, k.value * lin_x + b.value
 
 
 def diff(theor, real):
